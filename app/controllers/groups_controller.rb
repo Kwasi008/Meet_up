007bc14cbaac2b2b02174dc2d4340d639d42bdce
kwasi008 <Yeboah02208@gmail.com>
2022-04-04 16:54:02 -0400

    added crud for group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,54 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: %i[show edit update destroy]
 
   def index
     @groups = Group.all
   end
 
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to @group
+      flash[:notice] = 'Group was successfully created.'
+    else
+      redirect_to new_group_path
+      flash[:alert] = @group.errors.full_messages.to_s
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @group.update(group_params)
+      redirect_to @group
+      flash[:notice] = 'Group was successfully updated.'
+    else
+      redirect_to edit_group_path
+      flash[:alert] = @group.errors.full_messages.to_s
+    end
+  end
+
+  def destroy
+    if @group.destroy
+      redirect_to groups_path
+      flash.notice = 'Group has been deleted '
+    else
+      redirect_to groups_path
+      flash.alert = @vehicle.errors.full_messages.to_sentence
+    end
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
